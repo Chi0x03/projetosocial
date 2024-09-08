e3a46789a63728531a200632808dc2d8444589cd
Chi0x03 <Fodan13@protonmail.com>
2024-09-08 19:00:26 -0300

    exam controller: rename exam route

--- src/routes/examRoutes.js
+++ src/routes/examRoutes.js
@@ -8,6 +8,7 @@ router.get('/:id', isAuthenticated, ownsExam, examController.getExam);
 // router.get('/', isAuthenticated, ownsExam, examController.getAllExams);
 
 router.post('/', isAuthenticated, examController.createExam);
+router.post('/:id/rename', isAuthenticated, ownsExam, examController.renameExam);
 // router.post('/generate', isAuthenticated, examController.generateExam); // not implemented
 
 router.post('/:id/questoes', isAuthenticated, ownsExam, examController.addQuestionsToExam);
