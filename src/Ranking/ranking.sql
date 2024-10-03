84064fa87dd077bcd713a68b12c2e5fe82355a2e
Guilherme <111542995+UxieGu1@users.noreply.github.com>
2024-10-03 09:44:40 -0300

    Update ranking.sql

--- src/Ranking/ranking.sql
+++ src/Ranking/ranking.sql
@@ -1,1 +1,16 @@
+CREATE TABLE ranking (
+    id SERIAL PRIMARY KEY,
+    aluno_id INT NOT NULL,
+    prova_id INT NOT NULL,
+    acertos INT NOT NULL,
+    media_tempo_resposta DECIMAL(5, 2) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    UNIQUE (aluno_id, prova_id)
+);
+
+
+SELECT aluno_id, prova_id, acertos, media_tempo_resposta
+FROM ranking
+ORDER BY acertos DESC, media_tempo_resposta ASC;
+
 
