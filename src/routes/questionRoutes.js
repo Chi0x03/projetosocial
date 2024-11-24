093f76195c9ab9b0812f1b9bfcaa75d11bbe6a43
Chi0x03 <Fodan13@protonmail.com>
2024-11-24 02:11:08 -0300

    disabled auth for buceta

--- src/routes/questionRoutes.js
+++ src/routes/questionRoutes.js
@@ -4,9 +4,9 @@ const questionController = require('../controllers/questionController');
 const isAuthenticated = require('../middlewares/authMiddleware');
 
 router.post('/', isAuthenticated, questionController.createQuestion);
-router.post('/:id', isAuthenticated, questionController.editQuestion);
+router.post('/:id', /*isAuthenticated,*/ questionController.editQuestion);
 router.delete('/:id', questionController.deleteQuestion)
-router.get('/', isAuthenticated, questionController.getAllQuestions);
+router.get('/', /*isAuthenticated,*/ questionController.getAllQuestions);
 router.get('/:id',  questionController.getQuestion);
 router.get('/questao-by-descritor/:id', questionController.getQuestionsByDescritor);
 
